67975b689281c641ace28409a989d0888cfeff1e
bencebecsy <becsy.bence@gmail.com>
2022-10-06 09:34:54 -0700

    Fix runscript to expect the right number of returned arguments from the run + update some run parameters that are usually better to start with

--- run_bhb.py
+++ run_bhb.py
@@ -59,11 +59,11 @@ print(tspan / (365.25 * 24 * 3600))
 print(len(psrs))
 
 
-N=int(5e5)
+#N=int(5e5)
 #N=int(1e6)
-#N=int(1e3)
-T_max = 1.5
-n_chain = 3
+N=int(1e2)
+T_max = 8
+n_chain = 6
 
 ts_file = "../12p5yr-like_data/TauScans/tauscan_12p5yr-like_real4_RN.pkl"
 glitch_ts_file = "../12p5yr-like_data/TauScans/glitch_tauscan_12p5yr-like_real4_RN.pkl.pkl"
@@ -73,46 +73,49 @@ RN_start_file = "../12p5yr-like_data/RN_start_values.npz"
 
 savefile = "../Results/samples_12p5yr-like_real4.npz"
 
-samples, acc_fraction, swap_record, rj_record, ptas, log_likelihood = BayesHopperBurst.run_bhb(N, T_max, n_chain, psrs,
-                                                                    max_n_wavelet=5,
-                                                                    min_n_wavelet=0,
-                                                                    n_wavelet_start=1,
-                                                                    RJ_weight=4,
-                                                                    glitch_RJ_weight=4,
-                                                                    regular_weight=4,
-                                                                    noise_jump_weight=0,
-                                                                    PT_swap_weight=4,
-                                                                    tau_scan_proposal_weight=3,
-                                                                    glitch_tau_scan_proposal_weight=3,
-                                                                    gwb_switch_weight=0,
-                                                                    tau_scan_file=ts_file,
-                                                                    glitch_tau_scan_file=glitch_ts_file,
-                                                                    gwb_log_amp_range=[-18,-11],
-                                                                    rn_log_amp_range=[-18,-11],
-                                                                    wavelet_log_amp_range=[-10.0,-4.0],
-                                                                    per_psr_rn_log_amp_range=[-18,-11],
-                                                                    prior_recovery=False,
-                                                                    gwb_amp_prior='log-uniform',
-                                                                    rn_amp_prior='log-uniform',
-                                                                    wavelet_amp_prior='log-uniform',
-                                                                    per_psr_rn_amp_prior='log-uniform',
-                                                                    #gwb_on_prior=0.975,
-                                                                    max_n_glitch=5,
-                                                                    n_glitch_start=1,
-                                                                    glitch_log_amp_range=[-10.0,-4.0],
-                                                                    glitch_amp_prior='log-uniform',
-                                                                    t0_max=12.5,
-                                                                    draw_from_prior_weight=0, de_weight=0,
-                                                                    vary_white_noise=False,
-                                                                    include_gwb=False,
-                                                                    include_rn=True, vary_rn=True,
-                                                                    include_equad_ecorr=False,
-                                                                    wn_backend_selection=False,
-                                                                    noisedict_file=noisedict_file,
-                                                                    include_per_psr_rn=True,
-                                                                    vary_per_psr_rn=False,
-                                                                    #resume_from=None,
-                                                                    per_psr_rn_start_file=RN_start_file,
-                                                                    savefile=savefile, save_every_n=1000)
+results = BayesHopperBurst.run_bhb(N, T_max, n_chain, psrs,
+                                   max_n_wavelet=5,
+                                   min_n_wavelet=0,
+                                   n_wavelet_start=1,
+                                   RJ_weight=4,
+                                   glitch_RJ_weight=4,
+                                   regular_weight=4,
+                                   noise_jump_weight=0,
+                                   PT_swap_weight=4,
+                                   tau_scan_proposal_weight=3,
+                                   glitch_tau_scan_proposal_weight=3,
+                                   gwb_switch_weight=0,
+                                   tau_scan_file=ts_file,
+                                   glitch_tau_scan_file=glitch_ts_file,
+                                   gwb_log_amp_range=[-18,-11],
+                                   rn_log_amp_range=[-18,-11],
+                                   wavelet_log_amp_range=[-10.0,-4.0],
+                                   per_psr_rn_log_amp_range=[-18,-11],
+                                   prior_recovery=False,
+                                   gwb_amp_prior='log-uniform',
+                                   rn_amp_prior='log-uniform',
+                                   wavelet_amp_prior='uniform',
+                                   per_psr_rn_amp_prior='log-uniform',
+                                   #gwb_on_prior=0.975,
+                                   max_n_glitch=5,
+                                   n_glitch_start=1,
+                                   glitch_log_amp_range=[-10.0,-4.0],
+                                   glitch_amp_prior='uniform',
+                                   t0_max=12.5,
+                                   draw_from_prior_weight=0, de_weight=0,
+                                   vary_white_noise=False,
+                                   include_gwb=False,
+                                   include_rn=True, vary_rn=True,
+                                   include_equad_ecorr=False,
+                                   wn_backend_selection=False,
+                                   noisedict_file=noisedict_file,
+                                   include_per_psr_rn=True,
+                                   vary_per_psr_rn=False,
+                                   #resume_from=None,
+                                   per_psr_rn_start_file=RN_start_file,
+                                   savefile=savefile, save_every_n=1000)
 
-np.savez(savefile, samples=samples, acc_fraction=acc_fraction, swap_record=swap_record, log_likelihood=log_likelihood)
+samples, acc_fraction, swap_record, rj_record, ptas, log_likelihood, betas, PT_acc = results
+
+np.savez(savefile, samples=samples, acc_fraction=acc_fraction, swap_record=swap_record,
+                   log_likelihood=log_likelihood, betas=betas, PT_acc=PT_acc)
